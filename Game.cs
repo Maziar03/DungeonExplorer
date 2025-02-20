f94b681489428792b8420e5f8d8b8c2a1244ad7d
Maziar03 <maziyarrohani1382@gmail.com>
2025-02-20 18:46:43 +0000

    I’ve coded the Game class, and I'll make any edits if needed later.

--- Game.cs
+++ Game.cs
@@ -63,6 +63,7 @@ namespace DungeonExplorer
                 default:
                     Console.WriteLine("Error!Make sure to enter a valid choice");
                     break;
+
             }
         }
     }
