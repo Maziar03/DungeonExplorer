635a23135efb4ebcf158e94ae6b4bc3623677e56
Maziar03 <maziyarrohani1382@gmail.com>
2025-02-25 18:13:50 +0000

    The codes for the other classes have been added.

--- Room.cs
+++ Room.cs
@@ -3,10 +3,14 @@
     public class Room
     {
         private string description;
+        public string NextRoom { get; private set; }
+        public bool HasEnemy { get; set; }
 
-        public Room(string description)
+        public Room(string description, string nextRoom = null, bool hasEnemy = false)
         {
             this.description = description;
+            NextRoom = nextRoom;
+            HasEnemy = hasEnemy;
         }
 
         public string GetDescription()
